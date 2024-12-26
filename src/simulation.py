17ea0f1370e1f50b6330936bd5508c31f4c6a718
Vlad-Ber <vladik@live.se>
2024-12-26 15:10:14 +0100

    Import functions from plotfilter.py, added iir filters

--- src/simulation.py
+++ src/simulation.py
@@ -15,60 +15,97 @@ $ python3 simulation.py -b 010010000110100100100001
 
 import sys
 import numpy as np
-#from scipy import signal
-#import matplotlib.pyplot as plt
+from scipy import signal
+import matplotlib.pyplot as plt
 import wcslib as wcs
-
+from plotfilter import design_lowpass_filter, design_passband_filter, design_chebyshev1_bandpass, design_chebyshev1_lowpass
 
 def main():
-    # Parameters
-    # TODO: Add your parameters here. You might need to add other parameters as 
-    # well.
+    # -------------------------------------------------------------------------
+    # (A) Define your main system parameters
+    # -------------------------------------------------------------------------
+    fs = 48000          # Sampling rate (e.g., 48 kHz)
+    Tb = 0.04           # Symbol width in seconds (example)
+    fc = 4800           # Carrier frequency in Hz
+    channel_id = 19     # Example channel ID; adjust if needed.
 
-    #channel_id = ...   # Your channel ID
-    #Tb = ...           # Symbol width in seconds
-    #fs = ...           # Sampling frequency in Hz
-    # ...
+    # Filter specs
+    bp_order = 5
+    bp_rp    = 1.0
+    lp_order = 5
+    lp_rp    = 1.0
+    cutoff   = 50
 
     # Detect input or set defaults
     string_data = True
-    if len(sys.argv) == 2:
-        data = str(sys.argv[1])
-
-    elif len(sys.argv) == 3 and str(sys.argv[1]) == '-b':
+    args = sys.argv[1:]
+    if len(args) == 1:
+        data = str(args[0])
+    elif len(args) == 2 and args[0] == '-b':
         string_data = False
-        data = str(sys.argv[2])
-
+        data = str(args[1])
     else:
-        print('Warning: No input arguments, using defaults.', file=sys.stderr)
+        print('Warning: No valid input, using defaults.', file=sys.stderr)
         data = "Hello World!"
 
-    # Convert string to bit sequence or string bit sequence to numeric bit
-    # sequence
+    # -------------------------------------------------------------------------
+    # (B) Convert the input string -> bits -> baseband signal
+    # -------------------------------------------------------------------------
     if string_data:
         bs = wcs.encode_string(data)
     else:
-        bs = np.array([bit for bit in map(int, data)])
+        bs = np.array([int(bit) for bit in data])
 
-    # Encode baseband signal
     xb = wcs.encode_baseband_signal(bs, Tb, fs)
 
-    # TODO: Put your transmitter code here (feel free to modify any other parts
-    # too, of course)
+    # -------------------------------------------------------------------------
+    # (B1) Baseband-only decode test (NO channel, NO filters):
+    #      Convert xb to complex so decode_baseband_signal sees phase flips.
+    # -------------------------------------------------------------------------
+    yr_c = xb.astype(np.complex128)  # Make it complex: +1 -> phase=0, -1 -> phase=π
 
-    # Channel simulation
-    # TODO: Enable channel simulation.
-    #yr = wcs.simulate_channel(xt, fs, channel_id)
+    # Decode baseband
+    br = wcs.decode_baseband_signal(np.abs(yr_c), np.angle(yr_c), Tb, fs)
+    data_rx = wcs.decode_string(br)
 
-    # TODO: Put your receiver code here. Replace the three lines below, they
-    # are only there for illustration and as an MWE. Feel free to modify any
-    # other parts of the code as you see fit, of course.
-    yb = xb*np.exp(1j*np.pi/5) + 0.1*np.random.randn(xb.shape[0])
-    ybm = np.abs(yb)
-    ybp = np.angle(yb)
+    # -------------------------------------------------------------------------
+    # (C) TRANSMITTER: Modulate & Band-limit
+    # -------------------------------------------------------------------------
+    # 1) IQ up-conversion at carrier = 4.8 kHz
+    n = np.arange(len(xb))
+    x_mod = xb * np.cos(2.0 * np.pi * fc * n / fs)
 
-    # Baseband and string decoding
-    br = wcs.decode_baseband_signal(ybm, ybp, Tb, fs)
+    # 2) Bandpass filter
+    b_bp, a_bp = design_chebyshev1_bandpass(bp_order, bp_rp, fs, passband=(4750, 4850))
+    #b_bp, a_bp = design_passband_filter(fs=fs)
+    xt = signal.lfilter(b_bp, a_bp, x_mod)  # Transmitted signal
+
+    # -------------------------------------------------------------------------
+    # (D) CHANNEL SIMULATION
+    # -------------------------------------------------------------------------
+    # For now, bypass or keep the channel
+    yr = wcs.simulate_channel(xt, fs, channel_id)
+
+    # -------------------------------------------------------------------------
+    # (E) RECEIVER: Band-limiting -> IQ demod -> Lowpass -> Decode
+    # -------------------------------------------------------------------------
+    yr_f = signal.lfilter(b_bp, a_bp, yr)
+
+    # IQ demod
+    n_r = np.arange(len(yr_f))
+    yI_d = yr_f * np.cos(2.0 * np.pi * fc * n_r / fs)
+    yQ_d = -yr_f * np.sin(2.0 * np.pi * fc * n_r / fs)
+
+    # Lowpass each branch
+    b_lp, a_lp = design_chebyshev1_lowpass(order=lp_order, rp=lp_rp, fs=fs, cutoff=cutoff)
+    yI_b = signal.lfilter(b_lp, a_lp, yI_d)
+    yQ_b = signal.lfilter(b_lp, a_lp, yQ_d)
+
+    # Form complex baseband
+    yb = yI_b + 1j * yQ_b
+
+    # Decode baseband -> bits -> text
+    br = wcs.decode_baseband_signal(np.abs(yb), np.angle(yb), Tb, fs)
     data_rx = wcs.decode_string(br)
     print('Received: ' + data_rx)
 
